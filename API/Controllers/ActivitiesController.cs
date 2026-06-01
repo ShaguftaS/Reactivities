b45df550f1b0360dbd53a250c278b2a3ad894e4a
ShaguftaS <shaguftha.96@gmail.com>
2026-06-01 19:59:02 +0530

    Update

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -9,9 +9,19 @@ namespace API.Controllers;
 public class ActivitiesController(AppDbContext context) : BaseApiController
 {
     [HttpGet]
-
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
         return await context.Activities.ToListAsync();
+    }
+
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Activity>> GetActivityDetail(String id)
+    {
+        var activity = await context.Activities.FindAsync(id);
+
+        if (activity == null) return NotFound();
+
+        return activity;
     }
 }
--- Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6766d99c19f6252e89e550a182c73962667c4492")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac4854e133b7507f072885ef3dc5917881e0b2ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
