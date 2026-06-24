1852a3e2bb2fbd73f2e240d6cdbc25f73327598f
Shagufta S <shaguftha.96@gmail.com>
2026-06-24 05:31:26 -0400

    Section 2: END

--- Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bde37223111874b37199fecfdae6282a4d4e755")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9ea2991827343f33c839149c4a8f67e8b973e14")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
