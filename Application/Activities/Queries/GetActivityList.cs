35301318603bcaa74b17439c63f64f2917da14ca
Shagufta S <shaguftha.96@gmail.com>
2026-07-07 05:56:12 -0400

    End of Section 4

--- Application/Activities/Queries/GetActivityList.cs
+++ Application/Activities/Queries/GetActivityList.cs
@@ -12,7 +12,7 @@ public class GetActivityList
     public class Handler(AppDbContext context) : IRequestHandler<Query, List<Activity>>
     {
         public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
-        {
+        {            
             return await context.Activities.ToListAsync(cancellationToken);
         }
     }
